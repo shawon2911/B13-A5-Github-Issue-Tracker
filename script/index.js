5f8ceb506bf4092e6947785edd3a8a20afe1525c
shawon2911 <hosenuzzaman2018@gmail.com>
2026-03-09 04:08:19 +0600

    fetch and display all card from api

--- script/index.js
+++ script/index.js
@@ -15,6 +15,8 @@ const toggleStyle = (id) => {
     const selected = document.getElementById(id);
     selected.classList.remove('text-[#64748B]');
     selected.classList.add('btn-primary');
+
+    if(selected === )
 }
 
 // fetch all cards
@@ -48,17 +50,34 @@ const loadCards = () => {
 
 
 
+
 const displayCards = (items) => {
     const cardContainer = document.getElementById('card-container');
     cardContainer.innerHTML = "";
     items.forEach((item) => {
         console.log(item);
+        // check condition on priority 
+        let priorityBg = '' ;
+        let priorityColor = '';
+        if(item.priority == "high"){
+            priorityBg = 'bg-red-200';
+            priorityColor =  'text-[#EF4444]';
+        }
+        else if(item.priority == "medium"){
+            priorityBg = 'bg-orange-200';
+            priorityColor =  'text-[#D97706]';
+        }
+        else{
+            priorityBg = 'bg-gray-200';
+            priorityColor =  'text-[#64748B]';
+             
+        }
         const card = document.createElement('div');
         card.innerHTML = `
-            <div class="bg-white shadow-lg rounded-md py-4 px-4 space-y-4">
+             ${item.status == "open" ? `<div class="bg-white shadow-lg rounded-md py-4 px-4 space-y-4 border-t-7 border-green-500">` : `<div class="bg-white shadow-lg rounded-md py-4 px-4 space-y-4 border-t-7 border-purple-500">`}
                 <div class="flex justify-between items-center">
-                    <img src="assets/Open-Status.png" alt="">
-                    <div class="rounded-full h-6 w-20 flex justify-center items-center bg-red-200 font-medium text-[12px] text-[#EF4444]">${item.priority}</div>
+                    ${item.status == "open" ? `<img src="assets/Open-Status.png" alt="">` : `<img src="assets/Closed- Status .png" alt="">` }
+                    <div  class="rounded-full h-6 w-20 flex justify-center items-center font-medium text-[12px] ${priorityBg} ${priorityColor} ">${item.priority}</div>
                 </div>
 
                 <div class="space-y-4">
@@ -73,7 +92,7 @@ const displayCards = (items) => {
                 </div>
                 <div class="divider "></div>
                 <div class="space-y-3">
-                    <p class="text-[#64748B]">#1 by john_doe</p>
+                    
                     <p class="text-[#64748B]">#${item.id} by ${item.author}</p>
                     <p class="text-[#64748B]">${item.createdAt}</p>
                 </div>
