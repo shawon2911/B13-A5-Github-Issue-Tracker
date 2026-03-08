75a10e6de5b5c00799d59db5ad69e6850eed5578
shawon2911 <hosenuzzaman2018@gmail.com>
2026-03-09 02:46:30 +0600

    3 tabs and card count html part complete

--- script/index.js
+++ script/index.js
@@ -44,6 +44,10 @@ const loadCards = () => {
 //     "updatedAt": "2024-01-15T10:30:00Z"
 // }
 
+
+
+
+
 const displayCards = (items) => {
     const cardContainer = document.getElementById('card-container');
     cardContainer.innerHTML = "";
@@ -54,25 +58,29 @@ const displayCards = (items) => {
             <div class="bg-white shadow-lg rounded-md py-4 px-4 space-y-4">
                 <div class="flex justify-between items-center">
                     <img src="assets/Open-Status.png" alt="">
-                    <div class="rounded-full h-6 w-20 flex justify-center items-center bg-red-200 font-medium text-[12px] text-[#EF4444]">HIGH</div>
+                    <div class="rounded-full h-6 w-20 flex justify-center items-center bg-red-200 font-medium text-[12px] text-[#EF4444]">${item.priority}</div>
                 </div>
 
                 <div class="space-y-4">
-                    <h3 class="font-semibold  text-[#1F2937]">Fix navigation menu on mobile devices</h3>
-                    <p class="text-[14px] text-[#64748B]">The navigation menu doesn't collapse properly on mobile devices...</p>
+                    <h3 class="font-semibold  text-[#1F2937]">${item.title}</h3>
+                    <p class="text-[14px] text-[#64748B]">${item.description}</p>
                     <div>
-                        <span class="text-[#EF4444] bg-red-200 py-1 px-3 rounded-full">BUG</span>
-                        <span class="text-[#D97706] bg-orange-200 py-1 px-3 rounded-full">HELP WANTED</span>
+                        
+                        ${item.labels[0] ? `<span class="text-[#EF4444] bg-red-200 py-1 px-3 rounded-full">${item.labels[0]}</span>` : ''}
+                        ${item.labels[1] ? `<span class="text-[#D97706] bg-orange-200 py-1 px-3 rounded-full">${item.labels[1]}</span>` : ''}
+                        
                     </div>
                 </div>
                 <div class="divider "></div>
                 <div class="space-y-3">
                     <p class="text-[#64748B]">#1 by john_doe</p>
-                    <p class="text-[#64748B]">1/15/2024</p>
+                    <p class="text-[#64748B]">#${item.id} by ${item.author}</p>
+                    <p class="text-[#64748B]">${item.createdAt}</p>
                 </div>
                         
             </div>
         `
+        cardContainer.appendChild(card);
     })
 }
 loadCards();
